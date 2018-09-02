cfaaa7f920405620364cd827cdbd9d79a03afd69
wasifmiraz <wasifmiraz@mail.com>
2018-09-02 18:50:59 +0600

    fix

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const app = express();
 app.get('/', (req, res) => {
 res.send({ h1:'there'})
 });
-app.listen(process.env.PORT || 8080, () => {
+app.listen(process.env.PORT || 8080,() => {
  console.log("server running on: 8080")
 })
