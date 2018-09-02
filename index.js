05561ddbe5205a071dce8274b4760ab6c76b318e
wasifmiraz <wasifmiraz@mail.com>
2018-09-02 21:04:12 +0600

    token accss and profile callback

--- index.js
+++ index.js
@@ -1,19 +1,38 @@
 const express = require('express');
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').strategy;
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const Keys = require('./config/keys')
 
 const app = express();
 
-passport.use(new GoogleStrategy({
-    clientID:Keys.googleClientID,
-clientSecret:Keys.googleClientSecret,
-callbackURL:'/auth/google/callback'
- }, (accessToken) => {
-     console.log(accessToken);
+passport.use(
+    new GoogleStrategy({
 
- })
+    clientID:Keys.googleClientID,
+    clientSecret:Keys.googleClientSecret,
+    callbackURL:'/auth/google/callback',
+
+},
+ (accessToken,profile, refreshtoken) => {
+     console.log(accessToken);
+     console.log(profile);
+     console.log(refreshtoken);
+
+ }
+)
 );
+
+
+app.get('/auth/google', passport.authenticate('google', {
+scope:['profile','email']
+}));
+app.get('/auth/google/callback', passport.authenticate('google'));
+
+
+
+
+
+
 
 
 
