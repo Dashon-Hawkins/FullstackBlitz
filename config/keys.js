e08f7ae0374746bbf4349659483bbf83d4bd5f6d
wasifmiraz <wasifmiraz@mail.com>
2018-09-02 19:27:56 +0600

    keys hide and google id add

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    googleClientID:'237936609618-gtn26su307e87daavdrosm4lorqgua05.apps.googleusercontent.com',
+  googleSecretID:'NnVy9rH6hwTcWBjafCyWD478'
+
+}
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20');
+const GoogleStrategy = require('passport-google-oauth20').strategy;
 
 const app = express();
+
+passport.use(new GoogleStrategy());
 
 app.get('/', (req, res) => {
 res.send({ h1:'there'})
