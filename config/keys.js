bfdd2fae130e5d2395bdf17408444f42f39ad6e5
wasifmiraz <wasifmiraz@mail.com>
2018-09-03 13:27:12 +0600

    dev and prod set

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,8 @@
-module.exports = {
-    googleClientID:'237936609618-gtn26su307e87daavdrosm4lorqgua05.apps.googleusercontent.com',
-   googleClientSecret:'NnVy9rH6hwTcWBjafCyWD478',
-   mongoURL:'mongodb://miraz:password0@ds141942.mlab.com:41942/blitz-dev',
-   cookieKey:'sdfadsafsdafdsafsewqtqwdafsdafdsa'
+if(process.env.NODE_ENV === 'production')
+{
+
+}
+
+else {
 
 }
---
+++ config/prod.js
@@ -0,0 +1,7 @@
+module.exports = {
+    googleClientID:'237936609618-gtn26su307e87daavdrosm4lorqgua05.apps.googleusercontent.com',
+   googleClientSecret:'NnVy9rH6hwTcWBjafCyWD478',
+   mongoURL:'mongodb://miraz:password0@ds141942.mlab.com:41942/blitz-dev',
+   cookieKey:'sdfadsafsdafdsafsewqtqwdafsdafdsa'
+
+}
