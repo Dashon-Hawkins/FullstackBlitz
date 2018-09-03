b0665b0100216ba0288217df586b3126adaf9327
wasifmiraz <wasifmiraz@mail.com>
2018-09-03 21:03:00 +0600

    component visiable and exact

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,11 +5,15 @@ import {BrowserRouter, Route} from 'react-router-dom'
 const App = () => {
   const Header = () => <h2>Header</h2>
   const Survey = () =><h2>Survey</h2>
+  const Landing = () =><h2>Landing</h2>
 
   return(
     <BrowserRouter>
 <div>
- <Route path="/" component={Header} />
+<Header/>
+ <Route path="/" exact component={Landing}/>
+ <Route path="/survey/new" component={Survey}/>
+
 </div>
 
     </BrowserRouter>
