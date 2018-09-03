57b4bc22cd699bc5240c8ee422a210d7e9a56ddd
wasifmiraz <wasifmiraz@mail.com>
2018-09-03 18:24:11 +0600

    refactoring es7 passport js

--- services/passport.js
+++ services/passport.js
@@ -24,19 +24,16 @@ passport.use(
     callbackURL:'/auth/google/callback',
     proxy:true
 },
-(accessToken, refreshToken, profile, done) => {
-    console.log(profile);
-    User.findOne({googleId:profile.id})
-.then(existingUser => {
+ async (accessToken, refreshToken, profile, done) => {
+  const existingUser= await User.findOne({googleId:profile.id})
+
     if(existingUser) {
  done(null, existingUser)
     }
     else {
-        new User({ googleId:profile.id}).save()
-      .then(user => done(null, user));
+      const user =  await new User({ googleId:profile.id}).save()
+      done(null, user)
      }
-})
-
 
 })
 
