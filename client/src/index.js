7b30c2ca5d5f3ad78ac358515738a436f4e2e7db
wasifmiraz <wasifmiraz@mail.com>
2018-09-03 20:17:36 +0600

    redux set finish

--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,11 @@ import  React from 'react';
 import ReactDom from 'react-dom';
 import App from './components/App';
 import {Provider} from 'react-redux';
-import {createStore, applyMidlleware} from 'redux'
+import {createStore, applyMiddleware} from 'redux';
+import reducers from './components/reducers'
 
-const store =createStore(() =>[], {}, applyMidlleware())
+
+const store =createStore(reducers,{}, applyMiddleware())
 ReactDom.render(
 <Provider store={store}><App/></Provider>, document.querySelector('#root'));
 
