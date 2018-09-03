1ac22ac7b46a6e87680a3ecfdc7450d413a992cc
wasifmiraz <wasifmiraz@mail.com>
2018-09-03 14:57:06 +0600

    fixing heroku missmatch redirce

--- services/passport.js
+++ services/passport.js
@@ -22,6 +22,7 @@ passport.use(
     clientID:Keys.googleClientID,
     clientSecret:Keys.googleClientSecret,
     callbackURL:'/auth/google/callback',
+    proxy:true
 },
 (accessToken, refreshToken, profile, done) => {
     console.log(profile);
