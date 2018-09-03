1dca9561c5bde8b5f138f0c9f5448becc3934080
wasifmiraz <wasifmiraz@mail.com>
2018-09-03 20:25:11 +0600

    Route setup

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 
+import {BrowserRouter, Route} from 'react-router-dom'
+
 const App = () => {
+  const Header = () => <h2>Header</h2>
+  const Survey = () =><h2>Survey</h2>
 
   return(
-      <div>
-           hi app
-      </div>
+    <BrowserRouter>
+<div>
+ <Route path="/" component={Header} />
+</div>
+
+    </BrowserRouter>
+
   )
 
 };
