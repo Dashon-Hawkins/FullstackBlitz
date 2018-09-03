c3b12a31e92253da2e13e2bd2b7550f873a85e0f
wasifmiraz <wasifmiraz@mail.com>
2018-09-03 11:38:47 +0600

    existing

--- services/passport.js
+++ services/passport.js
@@ -2,13 +2,29 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const mongoose = require('mongoose');
 const Keys = require('../config/keys.js');
- const User = mongoose.model('users');
- passport.use(
+
+const User = mongoose.model('users');
+
+passport.use(
     new GoogleStrategy({
-     clientID:Keys.googleClientID,
+
+    clientID:Keys.googleClientID,
     clientSecret:Keys.googleClientSecret,
-    callbackURL:'/auth/google/callback'
-}, (accessToken, refreshToken, profile, done) => {
-   new User({googleId:profile.id}).save();
-}),
- )
+    callbackURL:'/auth/google/callback',
+},
+(accessToken, refreshToken, profile, done) => {
+    User.findOne({googleId:profile.id})
+.then(existingUser => {
+    if(existingUser) {
+ //we do not need added user this existing user
+ done(null, existingUser);
+    }
+    else {
+        new User({ googleId:profile.id}).save();
+    }
+})
+
+
+})
+
+);
