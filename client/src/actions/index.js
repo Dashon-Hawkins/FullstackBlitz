abf951a48bbee9230bf554999b0b9e0a2d9d00fe
wasifmiraz <wasifmiraz@mail.com>
2018-09-04 12:11:22 +0600

    action create with redux thunk

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,6 +2,10 @@ import axios from 'axios'
 import  {FETCH_USER} from './types'
 
 const fetchUser = () => {
-    axios.get('/api/current_user');
+    return function(dispatch) {
+    axios.get('/api/current_user')
+    .then(res => dispatch({ type:FETCH_USER, payload:res}))
+
+    }
 
 };
