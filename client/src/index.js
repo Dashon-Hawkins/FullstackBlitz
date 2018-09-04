aa8979c03e8b9c82ed1fcca6d9deba4cf5ae89e6
wasifmiraz <wasifmiraz@mail.com>
2018-09-04 11:40:40 +0600

    testing

--- client/src/index.js
+++ client/src/index.js
@@ -5,8 +5,6 @@ import App from './components/App';
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
 import reducers from './components/reducers'
-
-
 const store =createStore(reducers,{}, applyMiddleware())
 ReactDom.render(
 <Provider store={store}><App/></Provider>, document.querySelector('#root'));
