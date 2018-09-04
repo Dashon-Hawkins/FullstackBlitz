5284d02de62a50fbae065b7142d6b3237d79e6e4
wasifmiraz <wasifmiraz@mail.com>
2018-09-04 17:38:57 +0600

    user login logout testing with render content

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,6 +2,17 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux';
 
 class Header extends Component {
+  renderContent() {
+  switch(this.props.auth){
+    case  null :
+    return 'i am still deciding';
+    case false:
+return 'im logout';
+default:
+return 'i am loggedin';
+  }
+
+  }
 
     render() {
       console.log(this.props);
@@ -10,7 +21,7 @@ class Header extends Component {
             <div className="nav-wrapper">
               <a className="left brand-logo">Blitz</a>
               <ul className="right">
-                <li><a>Login with Google</a></li>
+                <li><a>{this.renderContent()}</a></li>
               </ul>
             </div>
           </nav>
