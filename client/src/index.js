fbbebf2c4858d531d807ee65060e2600980f4d41
wasifmiraz <wasifmiraz@mail.com>
2018-09-04 12:06:40 +0600

    additional proxy rules

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,8 @@ import App from './components/App';
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
 import reducers from './components/reducers'
-const store =createStore(reducers,{}, applyMiddleware())
+import reduxThunk from 'redux-thunk'
+const store =createStore(reducers,{}, applyMiddleware(reduxThunk))
 ReactDom.render(
 <Provider store={store}><App/></Provider>, document.querySelector('#root'));
 
