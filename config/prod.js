a11e933d2b4d1a8649f450d8f26f1d467f75d756
wasifmiraz <wasifmiraz@mail.com>
2018-09-05 17:03:20 +0600

    stripe secrect key set on dev and prod and heroku config

--- config/prod.js
+++ config/prod.js
@@ -2,6 +2,8 @@ module.exports = {
     googleClientID:process.env.GOOGLE_CLIENT_ID,
    googleClientSecret:process.env.GOOGLE_CLIENT_SECRET,
    mongoURL:process.env.MONOGO_URL,
-   cookieKey:process.env.COOKIEKEY
+   cookieKey:process.env.COOKIEKEY,
+   publisheableKey:process.env.PUBLISHEABLE_KEY,
+   stripeSecret:process.env.STRIPE_SECRET
 
 }
