015acf392a5757a95db8d10e764e65ccb72e6b0f
wasifmiraz <wasifmiraz@mail.com>
2018-09-06 19:22:23 +0600

    added billing

--- client/src/components/Payment.js
+++ client/src/components/Payment.js
@@ -13,7 +13,7 @@ class Payments extends Component {
         token={token =>this.props.handleToken(token)}
         stripeKey={process.env.REACT_APP_STRIPE_KEY}
       >
-        <button className="btn">
+        <button className="btn" style={{ background:'linear-gradient(to bottom, #3d7eaa, #ffe47a)'}}>
 PAY WITH CARD
         </button>
       </StripeCheckout>
