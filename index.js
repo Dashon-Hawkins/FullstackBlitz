d33988110bbef109f74179d8d2564f90e5931340
wasifmiraz <wasifmiraz@mail.com>
2018-09-06 21:15:09 +0600

    safdfsda

--- index.js
+++ index.js
@@ -31,28 +31,18 @@ app.use(passport.session());
 authRouter(app);
 BillingRouter(app);
 
-if(process.env.NODE_ENV === 'production') {
-
+if (process.env.NODE_ENV === 'production') {
+    // Express will serve up production assets
+    // like our main.js file, or main.css file!
     app.use(express.static('client/build'));
 
-    //express will serve up the index.html file
-    //if it doesn't recognize the route
-
+    // Express will serve up the index.html file
+    // if it doesn't recognize the route
     const path = require('path');
-
-//express will serve up the index.html file
-//if it doesn't recognize the route
-
     app.get('*', (req, res) => {
-
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-
-
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
-
-
-
-}
+  }
 
 
 
