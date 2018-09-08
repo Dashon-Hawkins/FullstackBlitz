c4b2f3c069a2df508b0f18ec398091ab8da4be68
wasifmiraz <wasifmiraz@mail.com>
2018-09-08 11:16:45 +0600

    disturctring req..body

--- routes/surveyRoute.js
+++ routes/surveyRoute.js
@@ -2,8 +2,8 @@ const RequireLogin = require('../middlewares/requireLogin');
 const RequireCredits= require('../middlewares/requireCredits')
 
 module.exports = app => {
-
     app.post('/api/surveys', RequireLogin,RequireCredits,(req, res) => {
+const {title, subject, body, recipients} = req.body;
 
     });
 
