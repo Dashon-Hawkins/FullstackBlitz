a128ee228839a20337e3e7c3054e67b6ad9197c9
wasifmiraz <wasifmiraz@mail.com>
2018-09-08 11:28:32 +0600

    survey added

--- routes/surveyRoute.js
+++ routes/surveyRoute.js
@@ -1,9 +1,25 @@
+
+const mongoose = require('mongoose';)
 const RequireLogin = require('../middlewares/requireLogin');
 const RequireCredits= require('../middlewares/requireCredits')
+
+const Survey = mongoose.model('surveys');
 
 module.exports = app => {
     app.post('/api/surveys', RequireLogin,RequireCredits,(req, res) => {
 const {title, subject, body, recipients} = req.body;
+
+const survey = new Survey({
+    title,
+    subject,
+    body,
+    recipients: recipients.split(',').map(email => ({ email: email.trim() })),
+_user:req.user.id,
+dateSent:Date.now()
+
+
+
+})
 
     });
 
