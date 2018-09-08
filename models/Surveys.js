8a1d899704feb786b6883191d32249a033ed5a42
wasifmiraz <wasifmiraz@mail.com>
2018-09-08 10:55:43 +0600

    recipients setup

--- models/Surveys.js
+++ models/Surveys.js
@@ -1,12 +1,14 @@
-const mongoose = require('mongoose');
-
+ const mongoose = require('mongoose');
 const {Schema} = mongoose;
+const RecipentSchema = require('./recipient');
 
 const surveySchema = new Schema ( {
     title:String,
     body:String,
     subject:String,
-    recipients:[String]
+    recipients:[RecipentSchema],
+    yes:{type:Number, default:0 },
+    no:{type:Number,default:0}
 });
 
 mongoose.model('surveys', surveySchema);
