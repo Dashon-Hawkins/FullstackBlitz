dcda02bf65839c880e65a3688be8a393d72e5772
wasifmiraz <wasifmiraz@mail.com>
2018-09-08 12:27:05 +0600

    mailer property setup

--- services/Mailler.js
+++ services/Mailler.js
@@ -3,7 +3,21 @@ const helper= sendGrid.mail;
 const keys = require('../config/keys');
 
 class Mailer extends helper.Mail {
+constructor({ subject, recipients}, content) {
+super();
+
+this.from_email = new helper.Email('no-reply@blitz.com');
+this.subject = subject;
+this.body = new helper.Content('text/html', content);
+this.recipients =this.formatAddresses(recipients);
+
 
 }
+
+
+
+}
+
+new Mailer(...)
 
 module.export = Mailer;
