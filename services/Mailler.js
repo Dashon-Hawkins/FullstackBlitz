887481533951b6941fc17cc563894bf761187f54
wasifmiraz <wasifmiraz@mail.com>
2018-09-08 12:49:21 +0600

    sending sendgrid mail set in mailer component

--- services/Mailler.js
+++ services/Mailler.js
@@ -6,6 +6,8 @@ class Mailer extends helper.Mail {
 constructor({ subject, recipients}, content) {
 super();
 
+this.sgApi = sendGrid(keys.sendGridKey);
+
 this.from_email = new helper.Email('no-reply@blitz.com');
 this.subject = subject;
 this.body = new helper.Content('text/html', content);
@@ -40,6 +42,19 @@ personalize.addTo(recipent);
     });
 
     this.addPersonalization(personalize);
+
+}
+
+async send() {
+
+ const request =this.sgApi.emptyRequest({
+ method:'POST',
+path:'/v3/mail/send',
+body:this.toJSON()
+ });
+
+ const response = this.sgApi.API(request);
+return response;
 
 }
 
