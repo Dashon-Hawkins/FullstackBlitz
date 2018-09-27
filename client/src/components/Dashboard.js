a79da5a92a853f3ebbd41c2c1401379ccdb952d5
wasifmiraz <wasifmiraz@mail.com>
2018-09-27 18:06:25 +0600

    adding Material Icon

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -6,6 +6,11 @@ const Dashboard = () =>
     return (
         <div>
         Dashboard
+<div className="fixed-action-btn">
+  <a className="btn-floating btn-large red">
+  <i className="material-icons">create</i>
+  </a>
+</div>
         </div>
 
     )
