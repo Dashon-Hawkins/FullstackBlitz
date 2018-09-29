ac5b4f78b11c99f0ceca7fdd3bbf35c6183cb955
wasifmiraz <wasifmiraz@mail.com>
2018-09-29 12:39:40 +0600

    refactoring custom field

--- client/src/components/Surveys/SurveyForms.js
+++ client/src/components/Surveys/SurveyForms.js
@@ -1,37 +1,60 @@
+import _ from 'lodash';
 import React, {Component} from 'react';
 import {reduxForm, Field} from 'redux-form'
 import SurveyFields from './SurveyFields'
 
+
+const FIELDS =
+ [
+{label:'Survey Title', name:'title'},
+{label:'Subjct Line', name:'subject'},
+{label:'Email Body', name:'body'},
+{label:'Recipient List', name:'emails'},
+ ]
+
 class SurveyForms extends Component {
     renderFields() {
-       return(
-   <div>
-   <Field
-   label="Survey Title"
-   type="text"
-   name="surveyTitle"
-   component={SurveyFields}
-   />
-   <Field
-   label="Subject Line"
-    type="text"
-   name="subjectLine"
-   component={SurveyFields}
-   />
-   <Field
-   label="Email Body"
-   type="text"
-   name="body"
-   component={SurveyFields}
-   />
-   <Field
-    label="Recipient List"
-    type="text"
-    name="recipient"
-    component={SurveyFields}
-   />
-   </div>
-       )
+          //disturcuering fileds travacy
+ return _.map(FIELDS, ({ label, name }) => {
+return <Field key={name} component ={SurveyFields} type="text" label={label} name={name}/>
+
+//generalway
+
+// return _.map(FIELDS, fields=> {
+//     return <Field key={fields.name} component ={SurveyFields} type="text" label={fields.label} name={fields.name}/>
+
+ })
+
+
+
+//        return(
+//    <div>
+//    <Field
+//    label="Survey Title"
+//    type="text"
+//    name="surveyTitle"
+//    component={SurveyFields}
+//    />
+//    <Field
+//    label="Subject Line"
+//     type="text"
+//    name="subjectLine"
+//    component={SurveyFields}
+//    />
+//    <Field
+//    label="Email Body"
+//    type="text"
+//    name="body"
+//    component={SurveyFields}
+//    />
+//    <Field
+//     label="Recipient List"
+//     type="text"
+//     name="recipient"
+//     component={SurveyFields}
+//    />
+//    </div>
+//        )
     }
 
     render () {
