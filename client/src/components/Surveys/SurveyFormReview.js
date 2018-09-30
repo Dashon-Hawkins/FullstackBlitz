83bb6dd133ba5d4afa054b4dcec30f6f3227fca9
wasifmiraz <wasifmiraz@mail.com>
2018-09-30 11:56:50 +0600

    review form value show testing

--- client/src/components/Surveys/SurveyFormReview.js
+++ client/src/components/Surveys/SurveyFormReview.js
@@ -1,15 +1,39 @@
 import React from 'react'
-const SurveyReview = (props) =>
+
+import {connect} from 'react-redux'
+const SurveyReview = ({ onCancel, formReviewValue}) =>
 {
+    console.log(formReviewValue)
     return (
         <div>
         <h1>Survey Form Review</h1>
+        <label>title</label>
+         <div>{formReviewValue.title}</div>
+         <label>subject</label>
+         <div>{formReviewValue.subject}</div>
+         <label>body</label>
+         <div>{formReviewValue.body}</div>
+         <label>emails</label>
+         <div>{formReviewValue.emails}</div>
+
         <button className="yellow darken-3 btn-flat"
-        onClick={props.onCancel}
+        onClick={onCancel}
         >Back </button>
         </div>
     )
 
 }
 
-export default SurveyReview
+function mapStateToProps(state) {
+    // console.log(state)
+    return {
+
+        formReviewValue:state.form.surveyForm.values
+
+    }
+
+}
+
+
+
+export default connect(mapStateToProps)(SurveyReview)
