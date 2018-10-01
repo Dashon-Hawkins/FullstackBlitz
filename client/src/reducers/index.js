ecc265cd8d8923e588eff07ede98640bc8f90b07
wasifmiraz <wasifmiraz@mail.com>
2018-10-01 11:50:06 +0600

    combine fetching surveys

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,9 +1,11 @@
 import {combineReducers} from 'redux';
 import {reducer as reduxform} from 'redux-form'
 import authReducers from './authReducers';
+import fetchingsurveysReducer from './fetchsurveysReducer'
 
 export default combineReducers({
     auth:authReducers,
-    form:reduxform
+    form:reduxform,
+    fetchingSurveys:fetchingsurveysReducer
 
 })
