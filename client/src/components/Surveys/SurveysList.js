3488e99bd9a07cabb2418c0f71c919057236499d
wasifmiraz <wasifmiraz@mail.com>
2018-10-01 12:32:52 +0600

    rendering surveylist

--- client/src/components/Surveys/SurveysList.js
+++ client/src/components/Surveys/SurveysList.js
@@ -7,11 +7,38 @@ class SurveysList extends Component {
     componentDidMount () {
          this.props.fetchSurveys();
     }
+renderSurveys() {
+    return this.props.surveyslist.map(survey =>
+                    {
+            return (
+            <div className="card darken-1" key={survey.id}>
+            <div className="card-content">
+            <span className="card-title">{survey.title}</span>
+            <p>
+            {survey.body}
+            </p>
+            <p className="right">
+            Sent On: {new Date(survey.dateSent).toLocaleDateString()}
+            </p>
+            </div>
+
+            <div className="card-action">
+            <a>YES:{survey.yes}</a>
+            <a>NO:{survey.no}</a>
+            </div>
+
+            </div>
+            )
+
+    })
+
+}
+
 
     render () {
          return (
               <div>
-               <h1>Surveylist</h1>
+              {this.renderSurveys()}
               </div>
          )
     }
@@ -20,6 +47,7 @@ class SurveysList extends Component {
 
 function mapStateToProps (state)
 {
+    console.log(state.fetchingSurveys)
     return {
         surveyslist:state.fetchingSurveys
     }
